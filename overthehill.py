d1a633d76987b288236e5a22a0b32074442bb58b
Paul Yang <yangaapaul@gum.rlogin>
2022-10-17 17:27:51 -0400

    overthehill

--- overthehill.py
+++ overthehill.py
@@ -1,34 +1,19 @@
-def transform(matrix, encryption,length):
-    result = []
-    for i in range(length):
-        total = 0; 
-        for j in range(length):
-            total += (matrix[j] * encryption[i][j])
-        result.append(total % 37)
-    return result
-def getKey(val,map):
-    for key, value in map.items():
-        if val == value:
-            return key
-    return "-1"
+
 
 n = int(input())
 encryption = [list(map(int, input().split())) for i in range (n)]
 message = input()
-for i in range(n-len(message)%n):
-    message+=' '
+rem = len(message) % n
+if rem > 0: 
+    message = message + ' ' * (n-rem)
 letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 '
 map = {letters[i]: i for i in range(len(letters))}
-shift = [map.get(message[i]) for i in range(len(message))]
 result = []
-i=0
-for i in range(n, len(shift)+1, n):
-    
-    result.append(transform(shift[i-n:i], encryption, n))
-    
-str = ''
-for i in range(len(result)):
+
+for i in range(len(message)//n):
+    chunk = message[n*i:n*(i+1)]
+    x = [map[l] for l in chunk]
     for j in range(n):
-        str+=getKey(result[i][j],map)
-print(str)
+        result.append(letters[sum(encryption[j][k] * x[k] for k in range(n) ) %37])
+print("".join(result))
 
