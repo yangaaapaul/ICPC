0f48998b59302d269abfd60c42b2009cea3d3752
Paul Yang <yangaapaul@perisimmon.rlogin>
2022-10-25 16:33:52 -0400

    forcedchoice

---
+++ forcedchoice.py
@@ -0,0 +1,9 @@
+x,y,z = map(int, input().split())
+for i in range(z):
+    line = input().split()
+    Found = False
+    for num in line[1:]:
+        if int(num) == y:
+            Found = True
+            break;
+    print("Keep" if Found else "REMOVE")
